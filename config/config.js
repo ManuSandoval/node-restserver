51e7aa2cf4e4254c09d1347166f329b9d4576406
Manuel Sand <manuelcarlossandoval@gmail.com>
2020-06-25 11:45:18 -0300

    conecta con atlas

--- config/config.js
+++ config/config.js
@@ -1,3 +1,16 @@
 //si está en producción corre el puerto del entorno (ej. heroku), y cuando esté en
 //desarrollo le asignará el 3000. Todo esto sin que el dev se entere
+//===========PUERTO===============
 process.env.PORT = process.env.PORT || 3000;
+
+//===========ENTORNO===============
+//HEROKU
+//NODE_ENV es una variable de Heroku
+process.env.NODE_ENV = process.env.NODE_ENV || "dev"; //si no existe => en desarrollo
+//===========BASE DE DATOS===============
+const URL_REMOTA =
+  "mongodb+srv://manucosa:EaXoXerucp6EYRHW@cluster0-tbyst.mongodb.net/cafe";
+const URL_LOCAL = "mongodb://localhost:27017/cafe";
+let urlDB;
+process.env.NODE_ENV === "dev" ? (urlDB = URL_LOCAL) : (urlDB = URL_REMOTA);
+process.env.URLDB = URL_REMOTA;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 require("../config/config.js");
+
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const bodyParser = require("body-parser");
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
 
 //estos 2 comandos son middlewares que parsean url-encoded => json
 //parsea application/x-www-form-urlencoded
@@ -9,28 +18,17 @@ app.use(bodyParser.urlencoded({ extended: false })); //.use se usa para definir 
 //parsea application/json
 app.use(bodyParser.json());
 
-app.get("/usuario", (req, res) => {
-  res.json("get Usuario");
-});
+app.use(require("../routes/usuario"));
 
-app.post("/usuario", (req, res) => {
-  let body = req.body;
-  body.nombre
-    ? res.json({ persona: body })
-    : res.status(400).json({
-        ok: false,
-        mensaje: "El nombre es necesario",
-      });
-});
-
-app.put("/usuario/:id", (req, res) => {
-  let id = req.params.id;
-  res.json({ id });
-});
-
-app.delete("/usuario", (req, res) => {
-  res.json("delete Usuario");
-});
+mongoose.connect(
+  process.env.URLDB, //SI LA BD NO EXISTE LA CREA
+  mongooseOptions,
+  (err) => {
+    err
+      ? console.log("La BD no conectó. Verifique que la BD esté up.")
+      : console.log("BD ONLINE.");
+  }
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listen at port ${process.env.PORT}`);
