75b71fae71cad3221e1ced41059be1bb08dfe84f
Manuel Sand <manuelcarlossandoval@gmail.com>
2020-07-01 17:11:40 -0300

    gitignore con config

--- config/config.js
+++ config/config.js
@@ -8,20 +8,19 @@ process.env.PORT = process.env.PORT || 3000;
 //NODE_ENV es una variable de Heroku
 process.env.NODE_ENV = process.env.NODE_ENV || "dev"; //si no existe => en desarrollo
 
-//===========DURACION DEL TOKEN===============
-//60"*60*24*30 = 30 días
-process.env.EXPIRES_IN = 60 * 60 * 24 * 30;
-
 //===========SEED (SECRET_KEY)===============
 process.env.SECRET_KEY = process.env.SECRET_KEY || "este-es-el-seed";
+//===========DURACION DEL TOKEN===============
+//60"*60*24*30 = 30 días
+process.env.EXPIRES_IN = process.env.EXPIRES_IN || 60 * 60 * 24 * 30;
 
 //===========ID GOOGLE (CLIENT_ID)===============
 process.env.CLIENT_ID =
   process.env.CLIENT_ID ||
   "710999825579-h7ftiuaofna7kvqvr4pn5akb1pc1lsas.apps.googleusercontent.com";
+
 //===========BASE DE DATOS===============
 const MONGO_LOCAL = "mongodb://localhost:27017/cafe";
-
 let URL_DB;
 process.env.NODE_ENV === "dev"
   ? (URL_DB = MONGO_LOCAL)
